0aa3160caa95efee081ba8a06ce66430de421b6d
finagle29 <finagle29@Milans-MBP.home>
2014-12-21 13:35:30 -0500

    first commit

---
+++ src/Card.java
@@ -0,0 +1,15 @@
+
+public class Card {
+
+	static final int CLUBS = 0;
+	static final int DIAMONDS = 1;
+	static final int HEARTS = 2;
+	static final int SPADES = 3;
+	
+	static final int ACE = 1;
+	static final int JACK = 11;
+	static final int QUEEN = 12;
+	static final int KING = 13;
+	
+	
+}
