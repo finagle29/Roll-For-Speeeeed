3b714485ff75837b46ee9d27f1a1289b7cfdbfa2
finagle29 <finagle29@Milans-MBP.home>
2014-12-21 19:16:10 -0500

    finalized Card class

--- src/Card.java
+++ src/Card.java
@@ -11,5 +11,54 @@ public class Card {
 	static final int QUEEN = 12;
 	static final int KING = 13;
 	
+	final int suit;
+	final int value;
+	
+	public Card( int thisValue, int thisSuit ) {
+		this.suit = thisSuit;
+		this.value = thisValue;
+	}
+	
+	public String valueToString() {
+		switch (value) {
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+			return Integer.toString(value);
+		case ACE:
+			return "Ace";
+		case JACK:
+			return "Jack";
+		case QUEEN:
+			return "Queen";
+		case KING:
+			return "King";
+		}
+		return null;
+	}
+	
+	public String suitToString() {
+		switch (suit) {
+		case CLUBS:
+			return "Clubs";
+		case DIAMONDS:
+			return "Diamonds";
+		case HEARTS:
+			return "Hearts";
+		case SPADES:
+			return "Spades";
+		}
+		return null;
+	}
+	
+	public String toString() {
+		return this.valueToString() + " of " + this.suitToString();
+	}
 	
 }
